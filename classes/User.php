a60d62e9e9f5c489591b658a28d3ae7cf947486a
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-09-18 21:58:01 +0200

    date affichage

--- classes/User.php
+++ classes/User.php
@@ -1,38 +1,47 @@
 <?php
 
-
 class User {
-  protected $id;
-  protected $pseudo;
-  protected $mdp;
-  
-  function __construct($pseudo, $mdp) {
-      $this->id = $id;
-      $this->pseudo = $pseudo;
-      $this->mdp = $mdp;
-  }
-  
-  function getId() {
-      return $this->id;
-  }
 
-  function getPseudo() {
-      return $this->pseudo;
-  }
+    protected $id;
+    protected $pseudo;
+    protected $mdp;
 
-  function getMdp() {
-      return $this->mdp;
-  }
+    function __construct($pseudo, $mdp, $id = NULL) {
+        $this->id = $id;
+        $this->pseudo = $pseudo;
+        $this->mdp = $mdp;
+    }
 
-  function setId($id) {
-      $this->id = $id;
-  }
+    function getId() {
+        return $this->id;
+    }
 
-  function setPseudo($pseudo) {
-      $this->pseudo = $pseudo;
-  }
+    function getPseudo() {
+        return $this->pseudo;
+    }
 
-  function setMdp($mdp) {
-      $this->mdp = $mdp;
-  }
+    function getMdp() {
+        return $this->mdp;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setPseudo($pseudo) {
+        $this->pseudo = $pseudo;
+    }
+
+    function setMdp($mdp) {
+        $this->mdp = $mdp;
+    }
+
+    function asHtml() {
+        return '<p>' . $this->pseudo . '</p>';
+    }
+
+    public function jsonSerialize() {
+        return get_object_vars($this);
+    }
+
 }
